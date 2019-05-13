8d95e2cd7008ee9bc9fd689b8431ab51b51ff158
SrideviSekar <47583185+SrideviSekar@users.noreply.github.com>
2019-05-12 18:25:09 -0700

    DAO Chnages

---
+++ DAOLayer/Model/ParentTask.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAOLayer.Model
+{
+    public class ParentTask
+    {
+        public int Parent_ID { get; set; }
+        public string Parent_Task { get; set; }
+    }
+}
---
+++ DAOLayer/Model/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DAOLayer.Model
+{
+    public class Task
+    {
+
+        public int Task_ID { get; set; }
+        public int? Parent_ID { get; set; }
+        public int Project_ID { get; set; }
+        public int User_ID { get; set; }
+        public string TaskName { get; set; }
+        public string ParentTaskName { get; set; }
+        public DateTime? Start_Date { get; set; }
+        public DateTime? End_Date { get; set; }
+        public int? Priority { get; set; }
+        public string Status { get; set; }
+    }
+}
